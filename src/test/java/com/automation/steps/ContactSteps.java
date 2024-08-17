d9d355606ba35d4db39604a8c138dbc55f765c4e
ManishSolanki.Khimjibhai <ManishSolanki.Khimjibhai@ust.com>
2024-08-17 15:15:20 +0530

    create Contact scenario added 3:15

---
+++ src/test/java/com/automation/steps/ContactSteps.java
@@ -0,0 +1,91 @@
+package com.automation.steps;
+
+import com.automation.pages.ContactPage;
+import com.automation.utils.ConfigReader;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.testng.Assert;
+
+public class ContactSteps {
+    ContactPage contactPage = new ContactPage();
+
+    @Given("user navigates to the Add to Contacts section")
+    public void user_navigates_to_the_section() {
+        contactPage.clickOnContactSection();
+    }
+
+    @When("user clicks on the Add to Contacts icon")
+    public void user_clicks_on_the_add_to_contacts_icon() {
+        contactPage.clickOnAddToContactIcon();
+    }
+
+    @Then("verify the Add Contact form is displayed")
+    public void verify_the_add_contact_form_is_displayed() {
+        Assert.assertTrue(contactPage.isFirstNameDisplayed());
+    }
+
+    @When("user enters {string} as the first name for the contact")
+    public void user_enters_as_the_first_name_for_the_contact(String string) {
+        if (string.equals("contact.firstname"))
+        {
+            contactPage.enterFirstName(ConfigReader.getConfigValue(string));
+
+        }else {
+            contactPage.enterFirstName(string);
+        }
+
+    }
+
+    @When("user enters {string} as the email for the contact")
+    public void user_enters_as_the_email_for_the_contact(String string) {
+        if (string.equals("contact.email"))
+        {
+            contactPage.enterEmail(ConfigReader.getConfigValue(string));
+
+        }else {
+            contactPage.enterEmail(string);
+        }
+    }
+
+    @When("user selects {string} as the timezone for the contact")
+    public void user_selects_as_the_timezone_for_the_contact(String string) {
+        if (string.equals("contact.timezone"))
+        {
+            contactPage.clickOnTimeZone();
+            contactPage.enterTimeZone(ConfigReader.getConfigValue(string));
+            contactPage.clickOnIndianTimeZone();
+
+        }else {
+            contactPage.clickOnTimeZone();
+            contactPage.enterTimeZone(string);
+            contactPage.clickOnIndianTimeZone();
+        }
+
+    }
+
+    @When("user enters {string} as the phone number for the contact")
+    public void user_enters_as_the_phone_number_for_the_contact(String string) {
+        if (string.equals("contact.phoneNo"))
+        {
+            contactPage.enterPhoneNumber(ConfigReader.getConfigValue(string));
+
+        }else {
+            contactPage.enterPhoneNumber(string);
+        }
+    }
+
+    @When("user clicks on the Save Contact button")
+    public void user_clicks_on_the_button() {
+        contactPage.clickOnSaveContactBtn();
+    }
+
+    @Then("verify the contact {string} is successfully added to the contact list")
+    public void verify_the_contact_is_successfully_added_to_the_contact_list(String string) {
+        string = ConfigReader.getConfigValue(string);
+        System.out.println(string);
+        Assert.assertTrue(contactPage.isContactSaved(string));
+    }
+
+
+}
