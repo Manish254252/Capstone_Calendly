29d6298426cfdd0fa734fe3fc4d52eaa56dbead8
Rahul <theredcar6@gmail.com>
2024-08-19 15:40:32 +0530

    updated downloaded file

---
+++ src/test/java/com/automation/pages/ProfilePage.java
@@ -0,0 +1,47 @@
+package com.automation.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ProfilePage extends BasePage{
+
+    @FindBy(xpath = "//div/h1")
+    WebElement profilePageTitle;
+
+    @FindBy(xpath = "//div[@data-component='avatar-editor']//label/span/input")
+    WebElement uploadPictureBtn;
+
+    @FindBy(xpath = "//div/button/span[contains(text(),'Apply')]")
+    WebElement applyBtn;
+
+    @FindBy(xpath = "//div[@data-component='avatar-editor']//img")
+    WebElement profileImage;
+
+    @FindBy(xpath = "//div/button/span[contains(text(),'Save')]")
+    WebElement saveChangesBtn;
+
+    @FindBy(xpath = "//div[@aria-live='polite']")
+    WebElement savedMsg;
+
+    public boolean isProfilePageDisplayed() {
+        return profilePageTitle.getText().equals("Profile");
+    }
+
+
+    public void clickOnUploadBtn() {
+        uploadPictureBtn.sendKeys(System.getProperty("user.dir")+"\\src\\test\\resources\\Images\\g1.jpg");
+        applyBtn.click();
+    }
+
+    public boolean isProfileChanged() {
+        return profileImage.getAttribute("src").contains("data:image/png");
+    }
+
+    public void clickOnSaveChangesButton() {
+        saveChangesBtn.click();
+    }
+
+    public boolean isChangesSaved() {
+        return savedMsg.getText().equals("Your settings have been saved!");
+    }
+}
---
+++ src/test/resources/features/Logout.feature
@@ -0,0 +1,12 @@
+Feature: Logout Functionality
+
+  Scenario: verify user can logout
+
+    Given user opens website
+    When user clicks on loginLink
+    Then verify user on login page
+    When user enters email as "userEmail"
+    And user clicks on loginBtn
+    And user enters password as "password"
+    Then user clicks on continue button
+    And verify user is on homepage
